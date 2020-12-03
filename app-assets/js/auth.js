6ecc5925b66bb6335db4ebde6a979180dde69526
jguz17 <johnathanguzman17@gmail.com>
2020-12-03 02:22:13 -0600

    fixed user reg bug

--- app-assets/js/auth.js
+++ app-assets/js/auth.js
@@ -3,6 +3,12 @@ let name = document.querySelector('#register-name')
 let email = document.querySelector('#register-email')
 let password = document.querySelector('#register-password')
 let password2 = document.querySelector('#register-password-2')
+let userAuth = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    user: null,
+    error: null
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -24,8 +30,33 @@ form.addEventListener('submit', (e) => {
             body: JSON.stringify(user)
         })
         .then((res) => res.json())
-        .then((data) => console.log(data))
-    }
+        .then((data) => {
+            if (data.token) {
 
-    form.reset()
+                localStorage.setItem('token', data.token)
+                userAuth.token = data.token
+                userAuth.isAuthenticated = true
+                userAuth.error = null
+
+            } else if (!data.token) {
+
+                localStorage.removeItem('token')
+                userAuth.isAuthenticated = false
+                userAuth.error = data.message
+                userAuth.token = null
+
+                name = document.querySelector('#register-name')
+                email = document.querySelector('#register-email')
+                password = document.querySelector('#register-password')
+                password2 = document.querySelector('#register-password-2')
+            }
+            form.reset()
+        })
+        
+    } 
+})
+
+document.querySelector('#trigger').addEventListener('click', () => {
+    console.log(userAuth)
+    console.log(localStorage)
 })
