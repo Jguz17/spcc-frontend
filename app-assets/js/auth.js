0bf743d24b4c6b31ae629bd50f42794e0b38b9b2
jguz17 <johnathanguzman17@gmail.com>
2020-12-03 05:34:48 -0600

    added new icon and localstorage check

--- app-assets/js/auth.js
+++ app-assets/js/auth.js
@@ -1,8 +1,13 @@
-let form = document.querySelector('.form-container form')
+let form = document.querySelector('#register')
 let name = document.querySelector('#register-name')
 let email = document.querySelector('#register-email')
 let password = document.querySelector('#register-password')
 let password2 = document.querySelector('#register-password-2')
+let loginForm = document.querySelector('#login')
+let loginEmail = document.querySelector('#login-email')
+let loginPassword = document.querySelector('#login-password')
+let loginSubmit = document.querySelector('#login-submit')
+
 let userAuth = {
     token: localStorage.getItem('token'),
     isAuthenticated: null,
@@ -10,53 +15,135 @@ let userAuth = {
     error: null
 }
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
+// create user
+if (form) {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+    
+        name = name.value
+        email = email.value
+        password = password.value
+        password2 = password2.value
+    
+        if (password === password2) {
+    
+            const user = { name, email, password }
+    
+            fetch('https://spcc-backend.herokuapp.com/api/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(user)
+            })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.token) {
+                    console.log(data)
+                    localStorage.setItem('token', JSON.stringify(data.token))
+                    userAuth.token = data.token
+                    userAuth.isAuthenticated = true
+                    userAuth.error = null
+                    loadUser()
+    
+                    setTimeout(function(){
+                        location.href = "../../index.html";
+                     },100)
+                    // window.location.href = '../../index.html'
+                } else if (!data.token) {
+    
+                    resetFields()
+                }
+                form.reset()
+            })
+            
+        } else {
+            console.log('Passwords do not match')
+        }
+    })
+}
 
-    name = name.value
-    email = email.value
-    password = password.value
-    password2 = password2.value
+// login user
+if (loginForm) {
+    loginForm.addEventListener('submit', (e) => {
+        e.preventDefault()
 
-    if (password === password2) {
+        loginEmail = loginEmail.value
+        loginPassword = loginPassword.value
+        
+        console.log(loginEmail)
+        console.log(loginPassword)
+        if (loginEmail && loginPassword) {
 
-        const user = { name, email, password }
+            const user = { loginEmail, loginPassword }
 
-        fetch('https://spcc-backend.herokuapp.com/api/users', {
-            method: 'POST',
+            fetch('https://spcc-backend.herokuapp.com/api/auth', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: user.loginEmail,
+                    password: user.loginPassword
+                })
+            })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.token) {
+                    localStorage.setItem('token', JSON.stringify(data.token))
+                    userAuth.token = data.token
+                    userAuth.isAuthenticated = true
+                    userAuth.error = null
+                    loadUser()
+    
+                    setTimeout(function(){
+                        location.href = "../../index.html";
+                     },100)
+                } else {
+                    resetFields()
+                }
+                loginForm.reset()
+            })
+        }
+    })
+} else if (!loginEmail.value && !loginPassword.value) {
+    console.log('Enter data')
+}
+
+// load user
+const loadUser = () => {
+    if (localStorage.token) {
+        fetch('https://spcc-backend.herokuapp.com/api/auth', {
             headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user)
+                'x-auth-token': localStorage.token
+            }
         })
         .then((res) => res.json())
         .then((data) => {
-            if (data.token) {
-
-                localStorage.setItem('token', data.token)
-                userAuth.token = data.token
-                userAuth.isAuthenticated = true
-                userAuth.error = null
-
-            } else if (!data.token) {
-
-                localStorage.removeItem('token')
-                userAuth.isAuthenticated = false
-                userAuth.error = data.message
-                userAuth.token = null
-
-                name = document.querySelector('#register-name')
-                email = document.querySelector('#register-email')
-                password = document.querySelector('#register-password')
-                password2 = document.querySelector('#register-password-2')
-            }
-            form.reset()
+            userAuth.user = data
         })
-        
-    } 
-})
+        .catch((error) => {
+            resetFields(error)
+        }) 
+    }
+}
+// clear fields
+const resetFields = (error) => {
+    localStorage.removeItem('token')
+    userAuth.isAuthenticated = false
+    userAuth.error = error.message
+    userAuth.token = null
+    userAuth.user = null
 
-document.querySelector('#trigger').addEventListener('click', () => {
-    console.log(userAuth)
-    console.log(localStorage)
-})
+    name = document.querySelector('#register-name')
+    email = document.querySelector('#register-email')
+    password = document.querySelector('#register-password')
+    password2 = document.querySelector('#register-password-2')
+    loginEmail = document.querySelector('#login-email')
+    loginPassword = document.querySelector('#login-password')
+}
+
+// document.querySelector('#trigger').addEventListener('click', () => {
+//     console.log(userAuth)
+//     console.log(localStorage)
+// })
